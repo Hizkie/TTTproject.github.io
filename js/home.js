d3203035c836143157da1bd4e774fdcd6514557a
TTTproject <53182357+TTTproject@users.noreply.github.com>
2019-09-05 17:08:57 +0200

    creating home.js file

---
+++ js/home.js
@@ -0,0 +1,6 @@
+function change(item){
+          var content = document.getElementById("Iframe");
+         
+         var theme = content.contentWindow.document.getElement
+        frame.contentWindow.postMessage(/*any variable or object here*/, '*');     
+   }
