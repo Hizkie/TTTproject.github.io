dbf46779d850d4854080dd42dfdb8226b7ab25db
TTTproject <53182357+TTTproject@users.noreply.github.com>
2019-09-22 21:23:56 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -133,9 +133,6 @@ function change_css(item){
 	loadingPages();
 	var content = document.getElementById("Iframe");
 	var sr = content.src; // uri of original file 
-			alert("this is outside " + item)
-
-	
 	if (htmlPages.includes(content.src)) {	
 		// AJAX REQUEST
 			//myReq = new XMLHttpRequest();	1
