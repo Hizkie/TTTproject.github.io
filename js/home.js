758cf5fdf89a4098d764743ff992cd1523ad426a
TTTproject <53182357+TTTproject@users.noreply.github.com>
2019-09-22 19:34:54 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -134,7 +134,6 @@ function change_css(item){
 	var content = document.getElementById("Iframe");
 	var href = content.src; // uri of original file 
 	
-
 	
 	if (htmlPages.includes(content.src)) {	
 		// AJAX REQUEST
@@ -147,6 +146,8 @@ function change_css(item){
 	// after the requeste completed to the stuff down alert()
 		// change also the CONTENT IFRAME with myReq.content.
 	//alert("req with status code complete: " + myReq.readyState);
+			document.getElementById("Iframe").href
+
      	        console.log("im change_css iv been called im gonna set css: " + item);
 		if (content.contentWindow.document.getElementById("style") !== null) {
 		content.contentWindow.document.getElementById("style").setAttribute("href",item);
